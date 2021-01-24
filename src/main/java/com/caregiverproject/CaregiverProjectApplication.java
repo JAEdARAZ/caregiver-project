ba3a86e682cc64ef9cad39782addfedbb340a1e4
Jaime Aranzazu <jaedaraz@gmail.com>
2021-01-24 20:19:55 +0100

    First commit

---
+++ src/main/java/com/caregiverproject/CaregiverProjectApplication.java
@@ -0,0 +1,13 @@
+package com.caregiverproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaregiverProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaregiverProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caregiverproject/CaregiverProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caregiverproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaregiverProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
